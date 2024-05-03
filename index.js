5c7c2f43e237da96e255456eb0541200bb27bb7e
Leroy0010 <167775533+Leroy0010@users.noreply.github.com>
2024-05-03 22:27:35 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+var counter = 0;
+
+var counts = [];
+
+var total = 0;
+
+$(".increment").on("click",function () {
+    counter++;
+    $(".count").text(counter);
+    $('.increment').addClass("pressed");
+    setTimeout(function () {
+        $(".increment").removeClass("pressed");
+    }, 300);
+});
+
+
+$(".save").on("click", function () {
+    counts.push(counter)
+    $(".num-count").append(counter + "  ");
+    counter = 0;
+    $(".count").text(counter);
+    $('.save').addClass("pressed");
+    setTimeout(function () {
+        $(".save").removeClass("pressed");
+    }, 300);
+}
+);
+
+$(".total").on("click", function () {
+    for (var index = 0; index < counts.length; index++) {total += counts[index];}
+    $(".p-total").text("The total is: " + total);
+    $('.total').addClass("pressed");
+    setTimeout(function () {
+        $(".total").removeClass("pressed");
+    }, 300);
+}
+);
